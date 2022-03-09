194aa3a4f4abe9e8a6cffcd35f6a764a319ff44f
George Laza <glaza@intelerad.com>
2022-03-08 20:28:33 -0500

    Correctly enable editor based on rang

--- src/app/editor-wrapper/editor-wrapper.component.ts
+++ src/app/editor-wrapper/editor-wrapper.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core'
 import { AngularEditorConfig } from '@kolkov/angular-editor'
+import { Observable, Subject, takeUntil } from 'rxjs'
 import { v4 as uuidv4 } from 'uuid'
 
 @Component({
@@ -7,68 +8,77 @@ import { v4 as uuidv4 } from 'uuid'
     templateUrl: './editor-wrapper.component.html',
     styleUrls: ['./editor-wrapper.component.scss']
 })
-export class EditorWrapperComponent implements OnInit {
+export class EditorWrapperComponent implements OnInit, OnDestroy {
 
-    @Input() editable!: boolean
+    @Input() editable$!: Observable<boolean>
     @Input() leiras!: string
     @Output() leirasChange = new EventEmitter<string>()
 
-    editorConfig!: AngularEditorConfig
+    editorConfig = defaultEditorConfig()
+    private destroy$ = new Subject<boolean>()
 
     readonly uuid = uuidv4()
 
-    constructor() {
+    ngOnInit(): void {
+        // TODO: Take until
+        this.editable$.pipe(
+            takeUntil(this.destroy$)
+        ).subscribe(editable => {
+            this.editorConfig.editable = editable
+            this.editorConfig.showToolbar = editable
+        })
     }
 
-    ngOnInit(): void {
-        this.configure()
+    ngOnDestroy(): void {
+        this.destroy$.next(true)
+        this.destroy$.complete()
     }
 
     changeLeiras(leiras: string) {
         this.leiras = leiras
         this.leirasChange.next(leiras)
     }
+}
 
-    configure() {
-        this.editorConfig = {
-            editable: this.editable,
-            spellcheck: true,
-            height: 'auto',
-            minHeight: '0',
-            maxHeight: 'auto',
-            width: 'auto',
-            minWidth: '0',
-            translate: 'yes',
-            enableToolbar: true,
-            showToolbar: this.editable,
-            placeholder: 'Ide írjál valamit...',
-            defaultParagraphSeparator: '',
-            defaultFontName: 'Comic Sans MS',
-            defaultFontSize: '5',
-            fonts: [
-                { class: 'arial', name: 'Arial' },
-                { class: 'times-new-roman', name: 'Times New Roman' },
-                { class: 'comic-sans-ms', name: 'Comic Sans MS' }
-            ],
-            sanitize: true,
-            toolbarPosition: 'top',
-            toolbarHiddenButtons: [
-                [
-                    'strikeThrough',
-                    'subscript',
-                    'superscript',
-                    'justifyLeft',
-                    'justifyCenter',
-                    'justifyRight',
-                    'justifyFull',
-                    'textColor',
-                    'backgroundColor',
-                    'customClasses',
-                    'insertImage',
-                    'removeFormat',
-                    'toggleEditorMode',
-                ]
+function defaultEditorConfig(): AngularEditorConfig {
+    return {
+        editable: true,
+        spellcheck: true,
+        height: 'auto',
+        minHeight: '0',
+        maxHeight: 'auto',
+        width: 'auto',
+        minWidth: '0',
+        translate: 'yes',
+        enableToolbar: true,
+        showToolbar: true,
+        placeholder: 'Ide írjál valamit...',
+        defaultParagraphSeparator: '',
+        defaultFontName: 'Comic Sans MS',
+        defaultFontSize: '5',
+        fonts: [
+            { class: 'comic-sans-ms', name: 'Comic Sans MS' },
+            { class: 'arial', name: 'Arial' },
+            { class: 'times-new-roman', name: 'Times New Roman' },
+        ],
+        sanitize: true,
+        toolbarPosition: 'top',
+        toolbarHiddenButtons: [
+            [
+                'strikeThrough',
+                'subscript',
+                'superscript',
+                'justifyLeft',
+                'justifyCenter',
+                'justifyRight',
+                'justifyFull',
+                'textColor',
+                'backgroundColor',
+                'customClasses',
+                'insertImage',
+                'removeFormat',
+                'toggleEditorMode',
             ]
-        }
+        ]
     }
 }
